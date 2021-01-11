244ab0b623984a1929c0b8dfa97561467b90429b
Evy Svensson <evy.svensson@stud.sti.se>
2021-01-11 09:08:20 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 int main(int argc, const char** argv) {
 	
 	//Credentials for MySql	
-	string url(argc >= 2 ? argv[1] : "127.0.0.1");
-	const string user(argc >= 3 ? argv[2] : "root");
-	const string pass(argc >= 4 ? argv[3] : "kaLLebANaN1967");
+	string url(argc >= 2 ? argv[1] : "insert_sql_url");
+	const string user(argc >= 3 ? argv[2] : "insert_sql_username");
+	const string pass(argc >= 4 ? argv[3] : "insert_password_to_sql_database_here");
 	const string database(argc >= 5 ? argv[4] : "environmentdata");
 
 	//Send the incoming devicevalues to a database in mySql
